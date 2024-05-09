99cac3cdf890da85fc000baa2ebaf085db99b67a
Daltochan <thedaltochan@gmail.com>
2024-05-09 14:08:46 +0200

    Basic raster layers added

--- main.js
+++ main.js
@@ -20,12 +20,15 @@ function init() {
     })
 
     const baseLayers = {
-        'OpenStreetMap': OpenStreetMap,
+        '<b>OpenStreetMap</b>': OpenStreetMap,
         'Stadia Maps': stadiaMaps
     }
 
-    const layerControls = L.control.layers(baseLayers, {}, {}).addTo(mymap)
+    const layerControl = L.control.layers(baseLayers, {}, {}).addTo(mymap)
 
+    const mapImage = "./data/ave.png";
+    const mapBounds = [[52.395534262108036, 16.947627303953514], [52.40958147969384, 16.997452020282616]]
+    const overlayMap = L.imageOverlay(mapImage, mapBounds).addTo(mymap)
 
     /* mymap.on('resize', function(e) {
         mymap.flyTo([0, 0]);
